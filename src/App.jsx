8f282e4347fcd89ba68f25970af4598be511d493
Stephanie Lin <slin.animation@gmail.com>
2025-12-19 18:15:22 -0500

    sets up front end components and react. .env path.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+const VITE_APP_BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL
+
+// axios.get(`${VITE_APP_BACKEND_URL}/boards`, {
+//     // ...
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <></>
+  )
+}
+
+export default App
