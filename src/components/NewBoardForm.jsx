403a59eeaaf8acc18f30cdd9c45e0d819428b484
Gina Song <gina0song@gmail.com>
2025-12-22 16:10:02 -0800

    updated NewBoardForm

--- src/components/NewBoardForm.jsx
+++ src/components/NewBoardForm.jsx
@@ -1,17 +1,63 @@
 import { useState } from 'react';
 
-const NewBoardForm = ({createNewBoard}) => {
-    const [title, setTitle] = useState('');
-    const [owner, setOwner] = useState('');
+const kDefaultBoardForm = {
+    title: '',
+    owner: ''
+};
+const NewBoardForm = ({ onFormSubmit }) => {
+    const [boardFormData, setBoardFormData] = useState(kDefaultBoardForm);
+
+    const makeControlledInput = (inputName, placeholder) => {
+        return <>
+        <input
+        type='text'
+        name={inputName}
+        id={`input-${inputName}`}
+        value={boardFormData[inputName]}
+        placeholder={placeholder}
+        onChange={updateFormChange}
+        required
+        />
+        </>
+    };
+
+const updateFormChange = (event) => {
+        const inputName = event.target.name;
+        const inputValue = event.target.value;
+        setBoardFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [inputName]: inputValue
+            };
+        });
+    console.log(inputName);
+    console.log(inputValue);
+    };
 
 const handleSubmit = (event) => {
         event.preventDefault();
-        const newBoard = {
-            title: title,
-            owner: owner
-        };
-        createNewBoard(newBoard);
-        setTitle('');
-        setOwner('');
-    }  
-}
+        onFormSubmit(boardFormData);
+        setBoardFormData(kDefaultBoardForm);
+    };
+return <>
+        <section className="new-board-form">
+            <h3>Create a New Board</h3>
+            
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="input-title">Title</label>
+                    {makeControlledInput('title', 'Enter board title...')}
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="input-owner">Owner</label>
+                    {makeControlledInput('owner', 'Your name...')}
+                </div>
+
+                <button type="submit">Create Board</button>
+            </form>
+        </section>
+    </>;
+};
+    
+    export default NewBoardForm;
