d231ba0961836cb2864458746632953f84435005
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-02 14:47:49 -0500

    coworking on 1/2
    Co-authored-by: Iris-Cheung-HY <Iris-Cheung-HY@users.noreply.github.com>
    Co-authored-by: gina0song <gina0song@users.noreply.github.com>
    Co-authored-by: stephaugi <stephaugi@users.noreply.github.com>

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,36 +1,31 @@
 import PropTypes from "prop-types";
 
-const Board = ({ board, onBoardSelect, isSelected }) => {
+const Board = ({ boardId, cardsData, addLikes }) => {
     // board = { board_id: 1, title: "...", owner: "..." }
     // onBoardSelect = function to call when clicked
     // isSelected = boolean, true if this board is currently selected
-    const handleClick = () => {
-        onBoardSelect(board.board_id);
-    };
 
+    const cards = cardsData.map((cardData) => {
+    return <Card 
+      key={cardData.id}
+      card={cardData}
+      onAddLike={addLikes}
+    />;
+    });
     return (
-        <div 
-            className={`board-item ${isSelected ? 'selected' : ''}`}
-            onClick={handleClick}
-        >
-            <h4>{board.title}</h4>
-            <p>Owner: {board.owner}</p>
-            {/* another way to display: */}
-            {/* <strong>{board.title}</strong> */}
-            {/* <small className="board-owner">by {board.owner}</small> */}
-        </div>
+        <>
+            {/* <h4>{board.title}</h4>
+            <p>Owner: {board.owner}</p> */}
+            {cards}
+        </>
     );
 };
 
 // PropTypes 
 Board.propTypes = {
-    board: PropTypes.shape({
-        board_id: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        owner: PropTypes.string.isRequired,
-    }).isRequired,
-    onBoardSelect: PropTypes.func.isRequired,
-    isSelected: PropTypes.bool.isRequired,
+    boardId: PropTypes.number.isRequired,
+    cardsData: PropTypes.array.isRequired,
+    addLikes: PropTypes.func.isRequired,
 };
 
 export default Board;
